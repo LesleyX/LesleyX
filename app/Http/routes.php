13c16d46c0b3d9116caefa817e0020aed981a384
Kristin Corona <kristincorona@gmail.com>
2016-02-26 09:07:13 -0500

    Fix routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,35 +11,7 @@
 |
 */
 
-Route::get('/', [
-	'uses'	=>	'\LesleyX\Http\Controllers\HomeController@index',
-	'as'	=>	'index'
-]);
 
-/**
- *  Authentication routes
- */
-Route::get('/auth/register', [
-    'uses' => '\LesleyX\Http\Controllers\AuthController@getRegister',
-    'as'   => 'auth.register',
-    'middleware' => ['guest']
-]);
- 
-Route::post('/auth/register', [
-    'uses' => '\LesleyX\Http\Controllers\AuthController@postRegister',
-    'middleware' => ['guest']
-]);
- 
-Route::get('/auth/signin', [
-    'uses' => '\LesleyX\Http\Controllers\AuthController@getLogin',
-    'as'   => 'auth.login',
-    'middleware' => ['guest']
-]);
- 
-Route::post('/auth/signin', [
-    'uses' => '\LesleyX\Http\Controllers\AuthController@postLogIn',
-    'middleware' => ['guest']
-]);
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -52,5 +24,33 @@ Route::post('/auth/signin', [
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get('/', [
+    'uses'  =>  '\LesleyX\Http\Controllers\HomeController@index',
+    'as'    =>  'index'
+    ]);
+
+    /**
+     *  Authentication routes
+     */
+    Route::get('/auth/register', [
+        'uses' => '\LesleyX\Http\Controllers\AuthController@getRegister',
+        'as'   => 'auth.register',
+        'middleware' => ['guest']
+    ]);
+     
+    Route::post('/auth/register', [
+        'uses' => '\LesleyX\Http\Controllers\AuthController@postRegister',
+        'middleware' => ['guest']
+    ]);
+     
+    Route::get('/auth/signin', [
+        'uses' => '\LesleyX\Http\Controllers\AuthController@getLogin',
+        'as'   => 'auth.login',
+        'middleware' => ['guest']
+    ]);
+     
+    Route::post('/auth/signin', [
+        'uses' => '\LesleyX\Http\Controllers\AuthController@postLogIn',
+        'middleware' => ['guest']
+    ]);
 });
