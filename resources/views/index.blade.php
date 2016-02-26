c26294579c9ed930e895a45802619330b939c924
Kristin Corona <kristincorona@gmail.com>
2016-02-26 08:53:23 -0500

    Update index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,43 @@
 @extends('layouts.master')
 
 @section('content')
-	<h1>LesleyX Project Management</h1>
+	@if(!Auth::check())
+		<h1>LesleyX Project Management</h1>
 
-	<p>LesleyX is a student-led collaborative initiative established to promote cross-disciplinary innovation and research. It is our mission to provide a transformative and enriching experience to individuals with an affinity for learning, a desire to innovate, and a passion for spreading ideas worth sharing.</p>
+		<p>LesleyX is a student-led collaborative initiative established to promote cross-disciplinary innovation and research. It is our mission to provide a transformative and enriching experience to individuals with an affinity for learning, a desire to innovate, and a passion for spreading ideas worth sharing.</p>
 
-	<p><img src="{{ asset('img/lx.gif') }}" /></p>
+		<p><img src="{{ asset('img/lx.gif') }}" /></p>
 
-	<a class="btn btn-large btn-info" href="/auth/register">Sign Up</a>
+		<a class="btn btn-large btn-info" href="/auth/register">Sign Up</a>
 
-	<p class="login">Already a member? <a class="btn btn-large btn-info" href="/auth/login">Login</a></p>
+		<p class="login">Already a member? <a class="btn btn-large btn-info" href="/auth/login">Login</a></p>
+	@endif
+
+	@if(Auth::check())
+		<div class="container-fluid">
+          <div class="row">
+            <div class="col-sm-3 col-md-2 sidebar">
+              <ul class="nav nav-sidebar">
+                <li style="margin-left:20px;">
+                    <img src="{{ Auth::user()->getAvatarUrl() }}" height="50" width="50" style="border-radius:25px;" />
+                </li>
+                <li><a href="#"> @ {{ Auth::user()->username }}</a></li>
+                <li class="active"><a href="#">LESLEYX<span class="sr-only">(current)</span></a></li>
+                <li><a href="#">Edit Account</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Todos</a></li>
+              </ul>
+              <ul class="nav nav-sidebar">
+                <li><a href="">Account</a></li>
+                <li><a href="">Help</a></li>
+                <li><a href="{{ route('auth.logout') }}">Sign Out</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+              <h1 class="page-header">Dashboard</h1>
+            </div>
+          </div>
+        </div>
+	@endif
 
 @stop
