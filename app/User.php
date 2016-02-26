99b3121aa19f883c7b3514c0a6c2d2af67b87aa1
Kristin Corona <kristincorona@gmail.com>
2016-02-26 08:51:12 -0500

    Update user model

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username', 'email', 'password',
     ];
 
     /**
@@ -23,4 +23,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getAvatarUrl()
+    {
+        return "http://www.gravatar.com/avatar/" . md5(strtolower(trim($this->email))) . "?d=mm&s=40";
+    }
 }
