cc290e103a5b557d68ddce324f6f87c59bd37d03
Kristin Corona <kristincorona@gmail.com>
2016-03-02 09:42:42 -0500

    Create projects directory in views; create new.blade.php

---
+++ resources/views/projects/new.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.master')
+
+@section('content')
+
+@include('layouts.partials.sidebar')
+
+	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+		<h1 class="page-header">New Project</h1>
+
+		<div class="col-lg-6">
+			<form class="form-vertical" role="form" method="post" action="">
+				<div class="form-group{{ $errors->has('status') ? 'has-error' : '' }}">
+					<label for="status" class="control-label">Project Status</label>
+					<select name="status" id="status">
+						<option value="">Choose a status</option>
+						<option value="Upcoming">Upcoming</option>
+						<option value="Active">Active</option>
+						<option value="Completed">Completed</option>
+					</select>
+					@if($errors->has('status'))
+						<span class="help-block">{{ $errors->first('status') }}</span>
+					@endif
+				</div>
+				<div class="form-group{{ $errors->has('name') ? 'has-error' : '' }}">
+					<label for="name" class="control-label">Name</label>
+					<input type="text" name="name" class="form-control" id="name" value="{{ old('name') ?: '' }}">
+					@if($errors->has('name'))
+						<span class="help-block">{{ $errors->first('name') }}</span>
+					@endif
+				</div>
+				<div class="form-group{{ $errors->has('due-date') ? 'has-error' : '' }}">
+					<label for="due-date" class="control-label">Due Date</label>
+					<input type="text" name="due-date" class="form-control" id="due-date" value="{{ old('due-date') ?: '' }}">
+					@if($errors->has('due-date'))
+						<span class="help-block">{{ $errors->first('due-date') }}</span>
+					@endif
+				</div>
+				<div class="form-group{{ $errors->has('notes') ? 'has-error' : '' }}">
+					<label for="notes" class="control-label">Notes</label>
+					<input type="text" name="notes" class="form-control" id="notes" value="{{ old('notes') ?: '' }}">
+					@if($errors->has('notes'))
+						<span class="help-block">{{ $errors->first('notes') }}</span>
+					@endif
+				</div>
+				<div class="form-group">
+	                <button type="submit" class="btn btn-default">Create</button>
+	            </div>
+	            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+			</form>
+		</div>
+	</div>
+
+@stop
