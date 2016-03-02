cd3d757e0d08729be4863de8ae79574cce259523
Kristin Corona <kristincorona@gmail.com>
2016-03-02 09:43:04 -0500

    Update index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,48 +1,24 @@
 @extends('layouts.master')
 
 @section('content')
-	@if(!Auth::check())
-		<h1>LesleyX Project Management</h1>
 
-		<p>LesleyX is a student-led collaborative initiative established to promote cross-disciplinary innovation and research. It is our mission to provide a transformative and enriching experience to individuals with an affinity for learning, a desire to innovate, and a passion for spreading ideas worth sharing.</p>
+@include('layouts.partials.sidebar')
+  
+  <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">  
+    @include('layouts.partials.alerts')   
+    <h1 class="page-header">Projects</h1>
 
-		<p><img src="{{ asset('img/lx.gif') }}" /></p>
+    <div class-"col-lg-6">     
 
-		<a class="btn btn-large btn-info" href="/auth/register">Sign Up</a>
+      @if($project)
 
-		<p class="login">Already a member? <a class="btn btn-large btn-info" href="/auth/login">Login</a></p>
-	@endif
+      @endif
 
-	@if(Auth::check())
-		<div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li style="margin-left:20px;">
-                <img src="{{ Auth::user()->getAvatarUrl() }}" height="50" width="50" style="border-radius:25px;" />
-            </li>
-            <li><a href="#"> @ {{ Auth::user()->username }}</a></li>
-            <li class="active"><a href="#">LESLEYX<span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Edit Account</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Todos</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Account</a></li>
-            <li><a href="">Help</a></li>
-            <li><a href="{{ route('auth.logout') }}">Sign Out</a></li>
-          </ul>
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Dashboard</h1>
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Dashboard</h1>
-          <h2 class="sub-header">Projects</h2>
-          <a href="btn btn-info" href="{{ route('projects.create') }}">New Project</a>
-        </div>
-      </div>
+      @if($project->isEmpty())
+        <h3>No Projects to display</h3>
+      @endif
     </div>
-	@endif
 
+    <a class="btn btn-info" href="{{ route('projects.create') }}">New Project</a>
+  </div>
 @stop
